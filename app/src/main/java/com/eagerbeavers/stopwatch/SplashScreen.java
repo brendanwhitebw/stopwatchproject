6f010995c76b71399acbda7c01a2cdb403697238
EoinBrennan <eoin.n.brennan@gmail.com>
2015-12-07 15:04:14 +0000

    Simplifying Splash code.

--- app/src/main/java/com/eagerbeavers/stopwatch/SplashScreen.java
+++ app/src/main/java/com/eagerbeavers/stopwatch/SplashScreen.java
@@ -18,9 +18,6 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = prefs.edit();
-
         Thread timer = new Thread() {
             public void run() {
                 try {
@@ -28,60 +25,15 @@ public class SplashScreen extends Activity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     Log.e(TAG, "Timer interrupted");
-                }
-                finally {
-                    prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
-                    SharedPreferences.Editor editor = prefs.edit();
-                    if (prefs.getBoolean("Timer", false)) {
-                        editor.remove("Timer");
-                        editor.apply();
-                    }
-                    Intent start = new Intent(SplashScreen.this,HomeScreen.class);
+                } finally {
+                    Intent start = new Intent(SplashScreen.this, HomeScreen.class);
                     startActivity(start);
                 }
             }
         };
 
-        if (!prefs.getBoolean("Timer", false)) {
-            editor.putBoolean("Timer", true);
-            editor.apply();
-            timer.start();
-        }
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = prefs.edit();
-
-        Thread timer = new Thread() {
-            public void run() {
-                try {
-                    sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Log.e(TAG, "Timer interrupted");
-                }
-                finally {
-                    prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
-                    SharedPreferences.Editor editor = prefs.edit();
-                    if (prefs.getBoolean("Timer", false)) {
-                        editor.remove("Timer");
-                        editor.apply();
-                    }
-                    Intent start = new Intent(SplashScreen.this,HomeScreen.class);
-                    startActivity(start);
-                }
-            }
-        };
-
-        if (!prefs.getBoolean("Timer", false)) {
-            editor.putBoolean("Timer", true);
-            editor.apply();
-            timer.start();
-        }
+        timer.start();
 
     }
 }
