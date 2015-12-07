c5efae23cc81625808f245d18c18dbfc5294829c
EoinBrennan <eoin.n.brennan@gmail.com>
2015-12-07 12:50:35 +0000

    Made the HomeScreen the actual map actvity, fixed small problems with the splash screen.

--- app/src/main/java/com/eagerbeavers/stopwatch/SplashScreen.java
+++ app/src/main/java/com/eagerbeavers/stopwatch/SplashScreen.java
@@ -2,10 +2,14 @@ package com.eagerbeavers.stopwatch;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class SplashScreen extends Activity {
+
+    SharedPreferences prefs;
 
     private final String TAG = "stopwatch.splash";
 
@@ -14,11 +18,8 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
 
         Thread timer = new Thread() {
             public void run() {
@@ -29,12 +30,58 @@ public class SplashScreen extends Activity {
                     Log.e(TAG, "Timer interrupted");
                 }
                 finally {
+                    prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
+                    SharedPreferences.Editor editor = prefs.edit();
+                    if (prefs.getBoolean("Timer", false)) {
+                        editor.remove("Timer");
+                        editor.apply();
+                    }
                     Intent start = new Intent(SplashScreen.this,HomeScreen.class);
                     startActivity(start);
                 }
             }
         };
 
-        timer.start();
+        if (!prefs.getBoolean("Timer", false)) {
+            editor.putBoolean("Timer", true);
+            editor.apply();
+            timer.start();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "Timer interrupted");
+                }
+                finally {
+                    prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
+                    SharedPreferences.Editor editor = prefs.edit();
+                    if (prefs.getBoolean("Timer", false)) {
+                        editor.remove("Timer");
+                        editor.apply();
+                    }
+                    Intent start = new Intent(SplashScreen.this,HomeScreen.class);
+                    startActivity(start);
+                }
+            }
+        };
+
+        if (!prefs.getBoolean("Timer", false)) {
+            editor.putBoolean("Timer", true);
+            editor.apply();
+            timer.start();
+        }
+
     }
 }
