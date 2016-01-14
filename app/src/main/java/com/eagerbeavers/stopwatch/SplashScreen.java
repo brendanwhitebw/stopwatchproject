9d8f1fd6f63ceedfd0ab90ed7c5c064f73102513
EoinBrennan <eoin.n.brennan@gmail.com>
2016-01-14 10:40:49 +0000

    Commented SplashScreen and it's layout files.

--- app/src/main/java/com/eagerbeavers/stopwatch/SplashScreen.java
+++ app/src/main/java/com/eagerbeavers/stopwatch/SplashScreen.java
@@ -7,16 +7,29 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+/* This activity appears when the user opens the app. It displays a Splash screen, a full screen
+ * image of the app's logo for an amount of time, then redirects the user to the main, or HomeScreen,
+ * activity.
+ */
+
 public class SplashScreen extends Activity {
 
-    SharedPreferences prefs;
+    // Tag used for console logs.
 
     private final String TAG = "stopwatch.splash";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // The layout is a fullscreen image of the app logo which takes up the entire screen
+
         setContentView(R.layout.activity_splash_screen);
+
+        /* When the app starts a seperate thread, a fork in the program flow is created.
+         * This thread runs a method with a timer, which runs for 3000 milliseconds (3 s),
+         * then initialises and uses and intent to direct the user to the HomeScreen.
+         */
 
         Thread timer = new Thread() {
             public void run() {
@@ -33,6 +46,10 @@ public class SplashScreen extends Activity {
             }
         };
 
+        /* Once the timer's contents and flow are determined, we have it initialised, the main
+         * program flow won't actually do anything else, so the user will simply see the logo for
+         * 3 seconds then be re-directed.
+         */
 
         timer.start();
 
